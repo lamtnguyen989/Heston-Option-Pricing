bb178b7f18708f52a0e62d01128e1d7f80ffb95c
Lam Nguyen <lamtnguyen989@gmail.com>
2026-01-15 15:52:24 -0600

    transfer the work

--- src/Heston-FFT.cc
+++ src/Heston-FFT.cc
@@ -2,7 +2,6 @@
 #include <Kokkos_Complex.hpp>
 #include <Kokkos_Random.hpp>
 #include <KokkosFFT.hpp>
-//#include <unistd.h>
 
 /* Macros */
 using Complex = Kokkos::complex<double>;
@@ -89,7 +88,7 @@ struct Diff_EV_config
 };
 
 // ------------------------------------------------------------------------------------ //
-/* Namespace for Compuitational routines */
+/* Compuitational routines */
 namespace Routines
 {
     KOKKOS_INLINE_FUNCTION double std_normal_cdf(double x)  {return 0.5 * (1 + Kokkos::erf(x * 0.70710678118654752));}
@@ -97,6 +96,7 @@ namespace Routines
     KOKKOS_INLINE_FUNCTION double black_scholes_call(double S, double K, double r, double sigma, double tau);
     KOKKOS_INLINE_FUNCTION double vega(double S, double K, double r, double sigma, double tau);
     KOKKOS_INLINE_FUNCTION double implied_volatility(double S, double K, double r, double tau, unsigned int max_iter, double epsilon);
+    Kokkos::View<double**> implied_volatility_surface(double S, Kokkos::View<double*> K, double r, Kokkos::View<double*> T);
 };
 
 /***
@@ -144,6 +144,23 @@ KOKKOS_INLINE_FUNCTION double Routines::implied_volatility(double S, double K, d
     return vol;
 }
 
+/*** 
+    Implied volatility surface paramterized routines
+***/
+Kokkos::View<double**> Routines::implied_volatility_surface(double S, Kokkos::View<double*> K, double r, Kokkos::View<double*> T)
+{
+    // Initialize variables
+    unsigned int n_strikes = K.extent(0);
+    unsigned int n_maturities = T.extent(0);
+    Kokkos::View<double**> iv_surface("iv_surface", n_strikes, n_maturities);
+
+    // Creating the implied_volatility surface
+
+    
+    // TODO
+    return iv_surface;
+}
+
 
 // ------------------------------------------------------------------------------------ //
 /* FFT solver object */
@@ -151,9 +168,16 @@ class Heston_FFT
 {
     public:
         /* Constructors */
-        Heston_FFT(double S, double r, Kokkos::View<double*> K, Kokkos::View<double*> T, HestonParameters(P))
+        Heston_FFT(double S, double r, Kokkos::View<double*> K, Kokkos::View<double*> T, HestonParameters P)
             : S(S) , r(r), strikes(K) , maturities(T) , params(P), fft_config(FFT_config())
         {}
+
+        /* Option Pricing functions */
+        Kokkos::View<double*> heston_call_prices(double t, bool print);
+        Kokkos::View<double**> heston_call_prices(bool print);
+
+        /* Calibrating parameters */
+
 
 
     private:
@@ -167,10 +191,50 @@ class Heston_FFT
 
 
         /* FFT pricing helpers */
-        KOKKOS_INLINE_FUNCTION Complex heston_characteristic(Complex u, double K, double t) const;
-        KOKKOS_INLINE_FUNCTION Complex damped_call(Complex v);
-        KOKKOS_INLINE_FUNCTION double heston_single_call();
+        KOKKOS_INLINE_FUNCTION Complex heston_characteristic(Complex u, double t) const;
+        KOKKOS_INLINE_FUNCTION Complex damped_call(Complex v, double t) const;
 };
+
+/***
+    Heston Characteristic functions 
+***/
+KOKKOS_INLINE_FUNCTION Complex Heston_FFT::heston_characteristic(Complex u, double t) const
+{
+    // A bunch of repeated constants in the calculation
+    Complex xi = params.kappa - i*params.rho*params.sigma*u;
+    Complex d = Kokkos::sqrt(square(xi) + square(params.sigma)*(square(u) + i*u));
+    if (Kokkos::real(d) < 0.0) { d = -d;}
+    //Complex g_1 = (xi + d) / (xi - d);
+    Complex g_2 = (xi - d) / (xi + d);
+
+    // Safe guard the fraction within the exponential terms 
+    Complex log_arg_frac = Kokkos::abs(1.0 - g_2) > EPSILON 
+                            ? (1.0 - g_2*Kokkos::exp(-d*t)) / (1.0 - g_2) 
+                            : Complex(EPSILON, EPSILON);
+
+    Complex last_term_frac = Kokkos::abs(1.0 - g_2*Kokkos::exp(-d*t)) > EPSILON
+                            ? (1.0 - Kokkos::exp(-d*t)) / (1.0 - g_2*Kokkos::exp(-d*t))
+                            : Complex(EPSILON, EPSILON);
+
+    // Exponent
+    Complex exponent = i*u*(Kokkos::log(S) + r*t)
+                    + (params.kappa*params.theta)/square(params.sigma) * ((xi -d)*t - 2.0*Kokkos::log(log_arg_frac))
+                    + (params.v0/square(params.sigma))*(xi-d)*(last_term_frac);
+
+    return Kokkos::exp(exponent);
+}
+
+/*** 
+    Damped call price function 
+***/
+KOKKOS_INLINE_FUNCTION Complex Heston_FFT::damped_call(Complex v, double t) const
+{
+    double alpha = fft_config.alpha;
+    Complex phi = heston_characteristic((v - i*(alpha + 1.0)), t);
+    return (Kokkos::exp(-r*t) * phi) / (square(alpha) + alpha - v*v +i*(2.0*alpha + 1.0)*v);
+}
+
+
 
 // ------------------------------------------------------------------------------------ //
 /* main() */
