fa4d7d45255e5423593b885f7bd5f76afd3623c5
Lam Nguyen <lamtnguyen989@gmail.com>
2026-01-20 12:03:41 -0600

    basic hard-setting values for eyes testing

--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,30 @@ int main (int argc, char* argv[])
 
     Kokkos::initialize(argc, argv);
     {
+	// Hard-setting basic parameters
+	double v0 = 0.04;
+      	double kappa = 2.0;
+       	double theta = 0.04;
+        double sigma = 0.3;
+       	double rho = -0.7;
+	HestonParameters param(v0, kappa, theta, sigma, rho);
+
+	// Hard setting basic strikes data
+	unsigned int n_strikes = 10;
+	Kokkos::View<double*> strikes("strikes", n_strikes);
+
+	// Hard-seeting basic maturities data
+	unsigned int n_maturities = 5;
+	Kokkos::View<double*> maturities("maturities", n_maturities);
+
+	// Create the solver based on these hard-setted data
+	double spot = 100.0;
+	double risk_free_rate = 0.03;
+	Heston_FFT solver(spot, risk_free_rate, strikes, maturities, param);
+
+	// Pricing surface
+	bool verbose = false;
+	solver.heston_call_prices(verbose);
 
     }
     Kokkos::finalize();
