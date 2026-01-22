79349fb2cd49f20e8f0076dca32e851f1fc90671
Lam Nguyen <lamtnguyen989@gmail.com>
2026-01-21 21:12:55 -0600

    Implement the logic for Diff Evo calibration

--- src/Heston-FFT.hpp
+++ src/Heston-FFT.hpp
@@ -11,9 +11,9 @@ struct HestonParameters
     double sigma;    // Vol of vol
 
     /* Constructors */
-    HestonParameters() {};
+    KOKKOS_INLINE_FUNCTION HestonParameters() {};
 
-    HestonParameters(double v_0, double kappa, double theta, double rho, double sigma)
+    KOKKOS_INLINE_FUNCTION HestonParameters(double v_0, double kappa, double theta, double rho, double sigma)
         : v0(v_0), kappa(kappa), theta(theta), rho(rho), sigma(sigma)
         {}
 };
@@ -99,7 +99,6 @@ class Heston_FFT
         Kokkos::View<double**> heston_call_prices(bool verbose) {return heston_call_prices(params, verbose);};
 
         /* Calibrating parameters */
-        double iv_surface_sq_loss(HestonParameters P);
         HestonParameters diff_EV_iv_surf_calibration(Kokkos::View<double**> iv_surface, Kokkos::View<double*> strikes, Kokkos::View<double*> maturities,
                                             ParameterBounds bounds, Diff_EV_config config, unsigned int seed);
 
@@ -331,17 +330,16 @@ HestonParameters Heston_FFT::diff_EV_iv_surf_calibration(Kokkos::View<double**> 
 
     // Variables and containers
     unsigned int population_size = config.population_size;
-    //Kokkos::Random_XorShift64_Pool<> rand_pool(seed);
-    //Kokkos::View<HestonParameters*> population("population", population_size);
-    //Kokkos::View<HestonParameters*> mutations("mutations", population_size);
-    //Kokkos::View<double*> population_losses("population_losses", population_size);
-    //Kokkos::View<double*> mutation_losses("mutation_losses", population_size);
-/*
-    // Initialize population
+    Kokkos::Random_XorShift64_Pool<> rand_pool(seed);
+    Kokkos::View<HestonParameters*> population("population", population_size);
+    Kokkos::View<HestonParameters*> mutations("mutations", population_size);
+    Kokkos::View<double*> population_losses("population_losses", population_size);
+    Kokkos::View<double*> mutation_losses("mutation_losses", population_size);
 
+    // Initialize population
     Kokkos::parallel_for("init_pop", population_size,
         KOKKOS_CLASS_LAMBDA(unsigned int k){
-                
+
             // Getting the random state
             Kokkos::Random_XorShift64_Pool<>::generator_type generator = rand_pool.get_state();
 
@@ -356,7 +354,117 @@ HestonParameters Heston_FFT::diff_EV_iv_surf_calibration(Kokkos::View<double**> 
             rand_pool.free_state(generator);
         });
     Kokkos::fence();
-*/
-    // TODO
-    return HestonParameters();   
+
+    // Evaluate inital population performance
+    Kokkos::MDRangePolicy<Kokkos::Rank<2>> surface_policy({0,0}, {n_strikes, n_maturities});
+    for (unsigned int p = 0; p < population_size; p++) {
+        double pop_iv_loss = 0;
+        Kokkos::View<double**> call_prices_surface_p = this->heston_call_prices(population(p), /*verbose=*/false);
+        Kokkos::View<double**> population_iv_surface_p = Routines::implied_volatility_surface(call_prices_surface_p, this->S, this->strikes, this->r, this->maturities);
+        Kokkos::parallel_reduce("init_pop_loss_calc", surface_policy, 
+            KOKKOS_CLASS_LAMBDA(unsigned int k, unsigned int t, double& local_iv_loss) {
+                local_iv_loss += square(population_iv_surface_p(k,t) - iv_surface(k,t));
+            }, pop_iv_loss);
+        Kokkos::fence();
+        population_losses(p) = pop_iv_loss;
+    }
+
+    // Differential Evaluation loop
+    for (unsigned int gen = 0; gen < config.n_gen; gen++) {
+
+        // Mutation generation
+        Kokkos::parallel_for("mutation_generation", population_size, 
+            KOKKOS_CLASS_LAMBDA(unsigned int p) {
+
+                // Initialize random state
+                Kokkos::Random_XorShift64_Pool<>::generator_type generator = rand_pool.get_state();
+
+                // Pick 3 distinct random candidate indices
+                int a, b, c;
+                do {a = static_cast<unsigned int>(generator.drand()*population_size);} while (a == p);
+                do {b = static_cast<unsigned int>(generator.drand()*population_size);} while (b == p || a == b);
+                do {c = static_cast<unsigned int>(generator.drand()*population_size);} while (c == p || c == b || c == a);
+
+                // Random dimensionality index (within 5 Heston parameters)
+                unsigned int R = static_cast<unsigned int>(generator.drand() * 5);
+
+                // Mutate of each dimension (parameters)
+                for (unsigned int j = 0; j < 5; j++) {
+
+                    if (generator.drand(0,1) < config.crossover_prob || j == R) {
+                        switch (j) {
+                            case 0: mutations(p).v0 = Kokkos::clamp(population(a).v0 + config.weight*(population(b).v0 - population(c).v0), 
+                                                                    bounds.v0_min, 
+                                                                    bounds.v0_max); 
+                                break;
+                            case 1: mutations(p).kappa = Kokkos::clamp(population(a).kappa + config.weight*(population(b).kappa - population(c).kappa), 
+                                                                    bounds.kappa_min, 
+                                                                    bounds.kappa_max);
+                                break;
+                            case 2: mutations(p).theta = Kokkos::clamp(population(a).theta + config.weight*(population(b).theta - population(c).theta), 
+                                                                    bounds.theta_min, 
+                                                                    bounds.theta_max);
+                                break;
+                            case 3: mutations(p).rho = Kokkos::clamp(population(a).rho + config.weight*(population(b).rho - population(c).rho), 
+                                                                    bounds.rho_min, 
+                                                                    bounds.rho_max);
+                                break;
+                            case 4: mutations(p).sigma = Kokkos::clamp(population(a).sigma + config.weight*(population(b).sigma - population(c).sigma), 
+                                                                    bounds.sigma_min, 
+                                                                    bounds.sigma_max);
+                                break;
+                        }
+                    } else {
+                        switch (j) {
+                            case 0: mutations(p).v0 = population(p).v0; break;
+                            case 1: mutations(p).kappa = population(p).kappa; break;
+                            case 2: mutations(p).theta = population(p).theta; break;
+                            case 3: mutations(p).rho = population(p).rho; break;
+                            case 4: mutations(p).sigma = population(p).sigma; break;
+                        }
+                    }
+                }
+
+                // Release the random state
+                rand_pool.free_state(generator);
+            });
+        Kokkos::fence();
+
+        // Evaluate the mutations
+        for (unsigned int m = 0; m < population_size; m++) {
+            double mut_iv_loss = 0;
+            Kokkos::View<double**> call_prices_surface_m = this->heston_call_prices(mutations(m), /*verbose=*/false);
+            Kokkos::View<double**> population_iv_surface_m = Routines::implied_volatility_surface(call_prices_surface_m, this->S, this->strikes, this->r, this->maturities);
+            Kokkos::parallel_reduce("init_pop_loss_calc", surface_policy, 
+                KOKKOS_CLASS_LAMBDA(unsigned int k, unsigned int t, double& local_iv_loss) {
+                    local_iv_loss += square(population_iv_surface_m(k,t) - iv_surface(k,t));
+                }, mut_iv_loss);
+            Kokkos::fence();
+            mutation_losses(m) = mut_iv_loss;
+        }
+
+        // Update population based on the mutations performance
+        Kokkos::parallel_for("update_population", population_size,
+            KOKKOS_CLASS_LAMBDA(unsigned int p) {
+                if (mutation_losses(p) < population_losses(p)) {
+                    population(p) = mutations(p);
+                    population_losses(p) = mutation_losses(p);
+                }
+            });
+    }
+
+    // Pick the best parameter from the population
+    Kokkos::MinLoc<double, unsigned int>::value_type bestLoc;
+    Kokkos::parallel_reduce("find_best_param", population_size,
+        KOKKOS_CLASS_LAMBDA(unsigned int p, Kokkos::MinLoc<double, unsigned int>::value_type& minLoc) {
+            double curr_loss = population_losses(p);
+            if (curr_loss < minLoc.val) {
+                minLoc.val = curr_loss;
+                minLoc.loc = p;
+            }
+        }, Kokkos::MinLoc<double, unsigned int>(bestLoc));
+
+
+    // Return
+    return population(bestLoc.loc);   
 }
