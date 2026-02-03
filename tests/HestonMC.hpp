062f75e5f6600969cb806cb07e2fb891f8815f1c
Lam Nguyen <lamtnguyen989@gmail.com>
2026-02-03 00:52:46 -0600

    configuring for testing

--- tests/HestonMC.hpp
+++ tests/HestonMC.hpp
@@ -1,7 +1,7 @@
 #include "headers.hpp"
 #include "Params-and-Configs.hpp"
 
-
+/* Monte Carlo simulation results container */
 struct MCResult
 {
     double price;       // Monte-Carlo result discounted back
@@ -10,31 +10,37 @@ struct MCResult
     double ci_upper;    // Confidence Interval Upper bound 
 };
 
-
+/* Class for Heston Model simulations */
 class HestonMC
 {
     public:
-
         /* Constructors */
-        HestonMC(double S0, double r, HestonParams P, unsigned int paths, unsigned int steps)
+        HestonMC(double S0, double r, HestonParameters P, unsigned int paths, unsigned int steps)
             : S_0(S0) , r(r) , params(P), n_paths(paths), n_steps(steps)
             {}
 
         /* Simulations */
-        MCResult single_EU_call(double K, double T, unsigned int steps, unsigned int paths, unsigned int seed);
+        MCResult single_EU_call_Milstein(double K, double T, unsigned int steps, unsigned int paths, unsigned int seed);
 
     private:
-        
         /* MC Data fields */
         double S_0;
         double r;
-        HestonParams params;
+        HestonParameters params;
         unsigned int n_steps;
         unsigned int n_paths;
 };
 
-MCResult HestonMC::single_EU_call(double K, double T, unsigned int steps, unsigned int paths=1e5, seed=12345)
+MCResult HestonMC::single_EU_call_Milstein(double K, double T, unsigned int steps, unsigned int paths=1e5, unsigned int seed=12345)
 {
-    // Initialize the random pool
+    // Initialize states
     Kokkos::Random_XorShift64_Pool<> rand_pool(seed);
+    MCResult result;
+
+    // TODO
+    result.price = 0.0;
+    result.std_dev = 0.0;
+    result.ci_upper = 0.0;
+    result.ci_lower = 0.0;
+    return result;
 }
---
+++ tests/tests.cc
@@ -0,0 +1,6 @@
+#include "HestonMC.hpp"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
