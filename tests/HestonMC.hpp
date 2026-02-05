bd6aaf7364bb0d06bb1640509402e3f86653089a
Lam Nguyen <lamtnguyen989@gmail.com>
2026-02-05 00:03:50 -0600

    polish the monte carlo code

--- tests/HestonMC.hpp
+++ tests/HestonMC.hpp
@@ -4,10 +4,9 @@
 /* Monte Carlo simulation results container */
 struct MCResult
 {
-    double discounted_price;    // Monte-Carlo result discounted back
-    double std_dev;             // Standard deviation of result
-    double ci_lower;            // Confidence Interval Lower bound
-    double ci_upper;            // Confidence Interval Upper bound 
+    double price;       // Monte-Carlo result discounted back
+    double ci_lower;    // Confidence Interval Lower bound
+    double ci_upper;    // Confidence Interval Upper bound 
 };
 
 /* Class for Heston Model simulations */
@@ -40,82 +39,63 @@ MCResult HestonMC::single_EU_call_Milstein(double K, double T, unsigned int step
     Kokkos::View<double*> S("price_process", paths);
     Kokkos::View<double*> V("variance_process", paths);
     Kokkos::parallel_for("init_processes", paths, 
-        KOKKOS_CLASS_LAMBDA(unsigned int p){
+        KOKKOS_CLASS_LAMBDA(unsigned int p) {
             S(p) = S_0;
             V(p) = params.v0;
         });
 
-    // Pre-compute random noises (to avoid potential race-condition later, tho look into more)
-    Kokkos::View<double**> Z_s("stock_noise", paths, steps);
-    Kokkos::View<double**> Z_v("variance_uncorrelated_noise", paths, steps);
-    
-    Kokkos::MDRangePolicy<Kokkos::Rank<2>> noise_gen_policy({0,0}, {paths, steps});
-    Kokkos::parallel_for("Pre_compute_uncorrelated_noise", noise_gen_policy, 
-        KOKKOS_CLASS_LAMBDA(unsigned int p, unsigned int t){
-            Kokkos::Random_XorShift64_Pool<>::generator_type generator = rand_pool.get_state();
-
-            Z_s(p,t) = generator.normal(0.0,1.0);
-            Z_v(p,t) = generator.normal(0.0,1.0);
-
-            rand_pool.free_state(generator);
-        });
-    Kokkos::fence();
-    Kokkos::parallel_for("correlating_noise", noise_gen_policy, 
-        KOKKOS_CLASS_LAMBDA(unsigned int p, unsigned int t) {
-            double Z_2 = Z_v(p,t);
-            Z_v(p,t) = params.rho*Z_s(p,t) + Kokkos::sqrt(1-square(params.rho))*Z_2;
-        });
-    Kokkos::fence();
-
-    // Note simulation is serial in time but parallel in paths (minus the noise correlation, but that is precomputed above)
+    // Note simulation is serial in time but parallel in paths
     for (unsigned int t = 0; t < steps; t++) {
         Kokkos::parallel_for("Milstein_step", paths, 
             KOKKOS_CLASS_LAMBDA(unsigned int p){
+
+                // Generating correlated random noise
+                Kokkos::Random_XorShift64_Pool<>::generator_type generator = rand_pool.get_state();
+                double Z_s = generator.normal(0.0, 1.0);
+                double Z_v = params.rho*Z_s + Kokkos::sqrt(1-square(params.rho)) * generator.normal(0.0, 1.0);
+                rand_pool.free_state(generator);
 
                 // Computing the evolution of the variance process
                 double V_curr = Kokkos::fmax(V(p), 0.0);
                 double V_next = V_curr
                             + params.kappa * (params.theta - V_curr) * dt
-                            + params.sigma * Kokkos::sqrt(V_curr * dt) * Z_v(p,t)
-                            + 0.25 * square(params.sigma) * (square(Z_v(p,t)) - 1.0) * dt;
+                            + params.sigma * Kokkos::sqrt(V_curr * dt) * Z_v
+                            + 0.25 * square(params.sigma) * (square(Z_v) - 1.0) * dt;
                 V(p) = Kokkos::fmax(V_next, 0.0);
 
                 // Computing the evolution of the price process (using current-time variance value, not updated one)
-                double S_curr = S(p);
-                S(p) = S_curr * Kokkos::exp((r - 0.5*V_curr)*dt + Kokkos::sqrt(V_curr * dt) * Z_s(p,t));
+                S(p) *= Kokkos::exp((r - 0.5*V_curr)*dt + Kokkos::sqrt(V_curr * dt) * Z_s);
         });
         Kokkos::fence();
     }
 
     // Reduce mean payoff
-    double mean_payoff = 0.0;
+    double mean_payoff;
     Kokkos::parallel_reduce("compute_sum_payoffs", paths, 
         KOKKOS_CLASS_LAMBDA(unsigned int p, double& local_sum) {
             local_sum += Kokkos::fmax(S(p) - K, 0.0);
-        }, mean_payoff);
-    Kokkos::fence();
+        }, Kokkos::Sum<double>(mean_payoff));
     mean_payoff /= paths;
 
-    // Reduce variance and standard deviation of payoffs
-    double variance = 0.0;
+    // Reduce variance of the payoffs
+    double variance;
     Kokkos::parallel_reduce("compute_variance", paths,
         KOKKOS_CLASS_LAMBDA(const unsigned int p, double& local_sum) {
-            double payoff_diff = Kokkos::fmax(S(p) - K, 0.0) - mean_payoff;
-            local_sum += square(payoff_diff);
-        }, variance);
-    Kokkos::fence();
+            local_sum += square(Kokkos::fmax(S(p) - K, 0.0) - mean_payoff);
+        }, Kokkos::Sum<double>(variance));
     variance /= static_cast<double>(paths - 1);
-    double std_error = Kokkos::sqrt(variance) / Kokkos::sqrt(static_cast<double>(paths));
+
+    // Compute the standard error from variance
+    double std_error = Kokkos::sqrt(variance) / Kokkos::sqrt(paths);
 
     // Compute the result
     double discount_factor = Kokkos::exp(-r * T);
     double ci_width = 1.96 * std_error * discount_factor;
 
     MCResult result;
-    result.discounted_price = discount_factor * mean_payoff;
-    result.std_dev = std_error * discount_factor;
-    result.ci_upper = result.discounted_price + ci_width;
-    result.ci_lower = result.discounted_price - ci_width;
+    result.price = discount_factor * mean_payoff;
+    result.ci_upper = result.price + ci_width;
+    result.ci_lower = result.price - ci_width;
     
     return result;
 }
--- tests/tests.cc
+++ tests/tests.cc
@@ -16,19 +16,21 @@ int main(int argc, char** argv)
         double sigma = 0.3;
         HestonParameters param(v0, kappa, theta, rho, sigma);
 
+        // Creating simulator object
         double spot = 100.0;
         double risk_free_rate = 0.03;
         HestonMC simulator(spot, risk_free_rate, param);
 
+        // Simulate a single vanilla call
         double strike = 85.0;
         double maturity = 1.0;
         unsigned int steps = 252;
         unsigned int paths = 3000;
-        unsigned int seed = 12345;
-        MCResult result = simulator.single_EU_call_Milstein(strike, maturity, steps, paths, seed);
+        MCResult result = simulator.single_EU_call_Milstein(strike, maturity, steps, paths);
 
-
-        Kokkos::printf("%f\n", result.discounted_price);
+        // Output to console for now
+        Kokkos::printf("Strike: %.2f, Maturity: %.2f\n", strike, maturity);
+        Kokkos::printf("%f\n", result.price);
         Kokkos::printf("95%% Confidence Interval: [%.2f , %.2f]\n", result.ci_lower, result.ci_upper);
     }
     Kokkos::finalize();
